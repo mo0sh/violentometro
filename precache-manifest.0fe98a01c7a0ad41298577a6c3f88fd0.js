4fa5c3e0e40dfe92dcdee64a775c2f0cc8515223
Oscar Javier Mosh <oscar@socialastronauts.com>
2019-09-12 00:32:29 -0500

    Updates

---
+++ precache-manifest.0fe98a01c7a0ad41298577a6c3f88fd0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82df2f05146f93b1d8952b3e305c0e80",
+    "url": "/violentometro/index.html"
+  },
+  {
+    "revision": "1a980744ec992d6c4060",
+    "url": "/violentometro/static/css/main.5e026243.chunk.css"
+  },
+  {
+    "revision": "3923096a40c2c9939c86",
+    "url": "/violentometro/static/js/2.0ece2bba.chunk.js"
+  },
+  {
+    "revision": "1a980744ec992d6c4060",
+    "url": "/violentometro/static/js/main.c7207bc3.chunk.js"
+  },
+  {
+    "revision": "0fcad22b55b7ec70e3e5",
+    "url": "/violentometro/static/js/runtime~main.877c3d03.js"
+  },
+  {
+    "revision": "81e2d17a27b771370eb4abd2269081c9",
+    "url": "/violentometro/static/media/devf-white.81e2d17a.png"
+  },
+  {
+    "revision": "e6bdbab63c1596aa544a0d33194ca274",
+    "url": "/violentometro/static/media/home.e6bdbab6.png"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/violentometro/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "1996c353cd289445bc14fc1e85721641",
+    "url": "/violentometro/static/media/red.1996c353.png"
+  }
+]);
